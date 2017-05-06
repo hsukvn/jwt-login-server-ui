ab1e2b84af3fe3b1c2397d54c8dc5237e0f863fa
kevinhsu <kevinhsu@synology.com>
2017-05-06 14:37:10 +0800

    Navigate if login success

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
-const ROOT_URL = 'http://localhost:3090';
+const ROOT_URL = 'http://192.168.56.102:3090'; // change to server ip
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
   // Sumit email/password to the server
-  axios.post(`${ROOT_URL}/signin`, { email, password }); // { email: email, password: password } in es6
-
-  // If request is good...
-  // - Update state to indicate user is authenticated
-  // - Save the JWT token
-  // - redirect to the route '/feature'
-
-
-  // If request is bad...
-  // - Show an error to the user
+  axios.post(`${ROOT_URL}/signin`, { email, password }) // { email: email, password: password } in es6
+    .then(response => {
+      // If request is good...
+      // - Update state to indicate user is authenticated
+      // - Save the JWT token
+      // - redirect to the route '/feature'
+      browserHistory.push('/feature');
+    })
+    .catch()(() => {
+      // If request is bad...
+      // - Show an error to the user
+    });
   }
 }
