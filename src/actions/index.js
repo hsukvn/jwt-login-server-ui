875ec8e2080d249a43411581b7553f99531ebafe
kevinhsu <kevinhsu@synology.com>
2017-05-06 20:17:09 +0800

    Implement signup action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,22 +10,34 @@ const ROOT_URL = 'http://192.168.56.102:3090'; // change to server ip
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-  // Sumit email/password to the server
-  axios.post(`${ROOT_URL}/signin`, { email, password }) // { email: email, password: password } in es6
-    .then(response => {
-      // If request is good...
-      // - Update state to indicate user is authenticated
-      dispatch({ type: AUTH_USER });
-      // - Save the JWT token
-      localStorage.setItem('token', response.data.token); // native in window object
-      // - redirect to the route '/feature'
-      browserHistory.push('/feature');
-    })
-    .catch(() => {
-      // If request is bad...
-      // - Show an error to the user
-      dispatch(authError('Bad Login Info'));
-    });
+    // Sumit email/password to the server
+    axios.post(`${ROOT_URL}/signin`, { email, password }) // { email: email, password: password } in es6
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        localStorage.setItem('token', response.data.token); // native in window object
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
+      });
+}
+}
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(response => dispatch(authError('response.data.error')));
   }
 }
 
